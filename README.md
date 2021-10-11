a63d5e777a8f094c79d18057baa0b2cd0ac3944a
KevinCristin <92331155+KevinCristin@users.noreply.github.com>
2021-10-11 13:22:54 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## Ej-Calculadora básica
+
+Proyecto básico de una calculadora aritmética para diversas prácticas.
+
+En su version inicial solo suma.
---
+++ app/src/main/java/calculadora/Calculadora.java
@@ -0,0 +1,9 @@
+package calculadora;
+
+public class Calculadora {
+
+    public double suma(double x, double y) {
+        return x + y;
+    }
+    
+}
